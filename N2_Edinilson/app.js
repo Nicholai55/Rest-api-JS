b2ea4b174f8d6f90cd8f09eab11c7c0b5f433de7
Nicholai55 <nicholai.gomes@faculdadecesusc.edu.br>
2021-05-25 18:13:33 -0300

    update
    refiz essa merda do zero

--- N2_Edinilson/app.js
+++ N2_Edinilson/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const User = require("./model/user");
+const Room = require("./model/room");
 
 require("dotenv/config");
 
@@ -9,25 +10,97 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("first request");
-});
-
-app.get("/users", (req, res) => {
-  let users = ["oawn", "super dude"];
-
-  res.send({
-    users: users,
-  });
-});
-
-app.post("/create_user", async (req, res) => {
+//lists all users
+app.get("/users", async (req, res) => {
   try {
-    const myuser = new User(req.body);
-    await myuser.save();
-    res.send(myuser);
+    const users = await User.find();
+    res.json(users);
   } catch (err) {
-    res.send({ message: err });
+    res.json({ message: err });
+  }
+});
+
+//create user
+app.post("/create_user", async (req, res) => {
+  const user = new User({
+    nome: req.body.nome,
+    sobrenome: req.body.sobrenome,
+  });
+
+  try {
+    const saveduser = await user.save();
+    res.json(saveduser);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//specific by name
+app.get("/users/:userName", async (req, res) => {
+  try {
+    const user = await User.findOne({ nome: req.params.userName });
+    res.json(user);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//delete user
+app.delete("/users/:userId", async (req, res) => {
+  try {
+    const removeUser = await User.remove({ _id: req.params.userId });
+    res.json(removeUser);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//SALAS
+//all rooms
+app.get("/rooms", async (req, res) => {
+  try {
+    const rooms = await Room.find();
+    res.json(rooms);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//create Room
+app.post("/create_room", async (req, res) => {
+  const room = new Room({
+    nome: req.body.nome,
+    lotacao: req.body.lotacao,
+  });
+
+  try {
+    const savedroom = await room.save();
+    res.json(savedroom);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//specific room
+app.get("/rooms/:roomId", async (req, res) => {
+  try {
+    const room = await Room.findById(req.params.roomId);
+    res.json(room);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//update room
+app.patch("/rooms/:roomId", async (req, res) => {
+  try {
+    const updatedRoom = await Room.updateOne(
+      { _id: req.params.roomId },
+      { $set: { nome: req.body.nome, lotacao: req.body.lotacao } }
+    );
+    res.json(updatedRoom);
+  } catch (err) {
+    res.json({ message: err });
   }
 });
 
--- N2_Edinilson/model/room.js
+++ N2_Edinilson/model/room.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
-const User = new mongoose.Schema({
+const Room = new mongoose.Schema({
   nome: {
     type: String,
     required: true,
   },
-  sobrenome: {
-    type: String,
+  lotacao: {
+    type: Number,
     required: true,
   },
 });
 
-module.exports = mongoose.model("user", User);
+module.exports = mongoose.model("room", Room);
